1795f102ef4307064ee806eb6b3aad98d68337b8
kmj0505 <menge0505@naver.com>
2023-02-01 20:01:18 +0900

    2. 확인문제

---
+++ Chapter2/Chap02Test12.java
@@ -0,0 +1,17 @@
+package minjichap02;
+
+public class Chap02Test12 {
+
+	public static void main(String[] args) {
+		//이름, 나이, 전화번호를 출력하시오
+		
+		String name = "감자바";
+		int age = 25;
+		String tel1 = "010", tel2 = "123", tel3 = "4567";
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("전화번호 : " + tel1 + '-' + tel2 + '-' + tel3);
+
+	}
+
+}
