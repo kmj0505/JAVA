76a5a61094428f5358deeff77f8fb9f6671e45f8
kmj0505 <menge0505@naver.com>
2023-02-01 10:11:11 +0900

    나머지 다 올렸다

---
+++ Chapter2/Chap02Test08.java
@@ -0,0 +1,15 @@
+package minjichap02;
+
+public class Chap02Test08 {
+
+	public static void main(String[] args) {
+		//결과가 어떤 숫자가 나올지 맞히시오
+		
+		double var1 = 3.5;
+		double var2 = 2.7;
+		int result = (int)var1 + (int)var2;
+		System.out.println(result);
+
+	}
+
+}
---
+++ Chapter2/Chap02Test13.java
@@ -0,0 +1,25 @@
+package minjichap02;
+
+import java.util.Scanner;
+
+public class Chap02Test13 {
+
+	public static void main(String[] args) {
+		// 첫 번째 수와 두 번째 수를 입력받아 덧셈하시오
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("첫 번째 수 : ");
+		String strNum1 = scanner.nextLine();
+		
+		System.out.print("두 번째 수 : ");
+		String strNum2 = scanner.nextLine();
+		
+		int num1 = Integer.parseInt(strNum1);
+		int num2 = Integer.parseInt(strNum2);
+		
+		int result = num1 + num2;
+		System.out.println("덧셈 결과 : " + result);
+	}
+
+}
